9636f11c0b1a1a15ab609a34a8c4d8ee77d6110d
leonardomorita <leonardomorita2@gmail.com>
2021-06-29 23:03:04 -0300

    Fixing the message box style

--- public/js/app.js
+++ public/js/app.js
@@ -3807,13 +3807,11 @@ Echo.channel('notifications').listen('UserSessionChanged', function (e) {
   var messageNotificationElement = document.getElementById('message-notification');
   messageNotificationElement.innerText = e.message;
   notificationElement.classList.remove('invisible');
-  notificationElement.classList.remove('bg-red-100 border border-red-400 text-red-700');
-  notificationElement.classList.remove('bg-green-100 border border-green-400 text-green-700');
 
   if (e.type == 'success') {
-    notificationElement.classList.add('bg-green-100 border border-green-400 text-green-700');
+    notificationElement.className = 'bg-green-100 border border-green-400 text-green-700';
   } else {
-    notificationElement.classList.add('bg-red-100 border border-red-400 text-red-700');
+    notificationElement.className = 'bg-red-100 border border-red-400 text-red-700';
   }
 });
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -9,12 +9,10 @@ Echo.channel('notifications')
 
         messageNotificationElement.innerText = e.message;
         notificationElement.classList.remove('invisible');
-        notificationElement.classList.remove('bg-red-100 border border-red-400 text-red-700');
-        notificationElement.classList.remove('bg-green-100 border border-green-400 text-green-700');
 
         if (e.type == 'success')  {
-            notificationElement.classList.add('bg-green-100 border border-green-400 text-green-700');
+            notificationElement.className = 'bg-green-100 border border-green-400 text-green-700';
         } else {
-            notificationElement.classList.add('bg-red-100 border border-red-400 text-red-700');
+            notificationElement.className = 'bg-red-100 border border-red-400 text-red-700';
         }
     });
