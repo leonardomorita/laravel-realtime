d1df7aea43e1f2fcdb82bebe240792615cbde372
leonardomorita <leonardomorita2@gmail.com>
2021-07-01 23:48:59 -0300

    Close #15

--- resources/views/game/show.blade.php
+++ resources/views/game/show.blade.php
@@ -28,7 +28,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="text-center flex justify-center">
-                        <img src="{{ asset('images/circle.png') }}" alt="Circle" id="circle" height="250" width="250" class="refresh">
+                        <img src="{{ asset('images/circle.png') }}" alt="Circle" id="circle" height="250" width="250">
                         <p id="winner" class="hidden"></p>
                     </div>
 
@@ -37,10 +37,10 @@
                     <div class="text-center">
                         <div class="p-2">
                             <label for="" class="">Your beat</label>
-                            <select name="" id="">
-                                <option value="" selected>Not in</option>
+                            <select name="" id="bet">
+                                <option selected>Not in</option>
                                 @foreach (range(1, 12) as $number)
-                                    <option value="">{{$number}}</option>
+                                    <option>{{ $number }}</option>
                                 @endforeach
                             </select>
                         </div>
@@ -61,6 +61,42 @@
     </div>
 
     <x-slot name="scripts">
-        <script></script>
+        <script>
+            const circleElement = document.getElementById('circle');
+            const timerElement = document.getElementById('timer');
+            const winnerElement = document.getElementById('winner');
+            const betElement = document.getElementById('bet');
+            const resultElement = document.getElementById('result');
+
+            Echo.channel('game')
+                .listen('RemainingTimeChanged', (e) => {
+                    timerElement.innerText = e.time;
+
+                    circleElement.classList.add('refresh');
+
+                    winnerElement.classList.add('hidden');
+
+                    resultElement.innerText = '';
+                    resultElement.classList.remove('text-green-500');
+                    resultElement.classList.remove('text-red-500');
+                })
+                .listen('WinnerNumberGenerated', (e) => {
+                    circleElement.classList.remove('refresh');
+                    winnerElement.classList.remove('hidden');
+
+                    let winnerNumber = e.number;
+                    winnerElement.innerText = winnerNumber;
+                    winnerElement.classList.remove('hidden');
+
+                    const bet = betElement[betElement.selectedIndex].innerText
+                    if (bet == winnerNumber) {
+                        resultElement.innerText = 'You win';
+                        resultElement.classList.add('text-green-500');
+                    } else {
+                        resultElement.innerText = `Sorry, the winner number is ${winnerNumber}`;
+                        resultElement.classList.add('text-red-500');
+                    }
+                });
+        </script>
     </x-slot>
 </x-app-layout>
